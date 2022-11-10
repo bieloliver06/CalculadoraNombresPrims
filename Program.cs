7add3ef107d23a0a0f4071a17818ac6e85518ee3
BuildTools <unconfigured@null.spigotmc.org>
2022-11-10 20:17:47 +0100

    your message

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@
                 {
                     try
                     {
-                        upTo = Convert.ToInt32(s_upTo);
+                        upTo = Math.Abs(Convert.ToInt32(s_upTo));
                         break;
                     }
                     catch (System.FormatException)
