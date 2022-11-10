3a7782c95e21b7588874834cdee262e860d60f0b
BuildTools <unconfigured@null.spigotmc.org>
2022-11-10 19:41:29 +0100

    your message

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ActivitatVoluntaria
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            int upTo;
+            while (true)
+            {
+                Console.WriteLine("Introdueix fins a quin nombre vols saber si es prim : ");
+                string? s_upTo = Console.ReadLine();
+                if (s_upTo != null)
+                {
+                    try
+                    {
+                        upTo = Convert.ToInt16(s_upTo);
+                        break;
+                    }
+                    catch (System.FormatException)
+                    {
+                        Console.WriteLine("El que has introduit no és valid");
+                    }
+                }
+            }
+            for (int i = 2; i <= upTo; i++)
+            {
+                IsPrime(i);
+            }
+        }
+        static bool IsPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0 && i != number) return false;
+
+            }
+            Console.WriteLine($"El nombre {number} es prim");
+            return true;
+        }
+    }
+}
